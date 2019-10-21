23bffe4e8995478a49d2e58c14f35b277b739bff
hungmuha <hung.soboss@gmail.com>
2019-10-20 20:03:41 -0600

    getting some work done in the linkedlist section

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,75 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data,next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    insertFirst(data) {
+        this.head = new Node(data,this.head);
+    }
+    size() {
+        let count = 0;
+        let node = this.head;
+            while(node) {
+                count++;
+                node = node.next;
+            }
+        return count;
+    }
+    getFirst() {
+        return this.head;
+    }
+    getLast() {
+        let node = this.head;
+            while(node.next) {
+                node = node.next;
+            };
+        return node;
+    }
+    clear() {
+        this.head = null;
+    }
+    removeFirst() {
+        if(!this.head) {
+            return;
+        }
+        this.head = this.head.next;
+    }
+    removeLast() {
+        let previous = this.head;
+        if(!previous) {
+            return;
+        } 
+        
+        if (previous && !previous.next) {
+            this.head = null;
+            return;
+        } 
+
+        let node = this.head.next;
+        while(node.next) {
+            previous = node;
+            node = node.next;
+        };
+        previous.next = null;  
+    }
+    insertLast(data) {
+        const lastNode = this.getLast();
+        if(lastNode) {
+            lastNode.next = new Node(data);
+        }else {
+            this.head = new Node(data);
+        };
+        
+    }
+}
 
 module.exports = { Node, LinkedList };
--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,40 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// function pyramid(n) {
+//     var totalRow = n;
+//     var totalColumn = 2*n-1;
+//     var center = Math.floor(totalColumn/2);
+//     for(let row=0; row<totalRow; row++) {
+//         let print = '';
+//         for(let column=0; column<totalColumn; column++) {
+//             if(column>=center-row && column<=center+row) {
+//                 print+='#';
+//             } else {
+//                 print+=' ';
+//             }
+//         }
+//         console.log(print);
+//     }
+// }
+
+//Recursion
+function pyramid(n, row=0,level='') {
+    totalcolumn = 2*n-1;
+    center=Math.floor(totalcolumn/2);
+    if(n===row){
+        return;
+    }
+    if(level.length === totalcolumn) {
+        console.log(level);
+        return pyramid(n,row+1);
+    }
+    if(level.length>=center-row && level.length<=center+row){
+        level+="#";   
+    }else {
+        level+=" ";
+    }
+    pyramid(n,row,level);
+}
 
 module.exports = pyramid;
