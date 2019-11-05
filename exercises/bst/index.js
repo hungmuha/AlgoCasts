5810697b9f83f1643d534f86d6870c6fd3d55a41
hungmuha <hung.soboss@gmail.com>
2019-11-04 18:53:47 -0700

    updated all answer to bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,37 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+    insert(data) {
+        // let node = arguments[1]? arguments[1] : this;
+        // if(data < node.data) {    
+        //     if(node.left) {
+        //         node.insert(data,node.left);
+        //     } else {
+        //         node.left = new Node(data);
+        //     }   
+        // } else if(data > node.data) {
+        //     if(node.right) {
+        //         node.insert(data,node.right);
+        //     } else {
+        //         node.right = new Node(data);
+        //     }
+        // }
+        if(data < this.data && this.left) {
+            this.left.insert(data);
+        } else if (data < this.data) {
+            this.left = new Node(data);
+        } else if (data > this.data && this.right) {
+            this.right.insert(data);
+        } else if (data > this.data) {
+            this.right = new Node(data);
+        }
+    }
+}
 
 module.exports = Node;
--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,34 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    // let arr = [root];
+    // let result = [1];
+    // let levelCounter = 1;
+    // while(arr.length) {
+    //     if(levelCounter==0) {
+    //         levelCounter = arr.length;
+    //         result.push(arr.length);
+    //     }
+    //     let element = arr.shift();
+    //     arr.push(...element.children);
+    //     levelCounter--;
+    // }
+    // return result;
+
+    let arr = [root, 's'];
+    let result = [0];
+    while(arr.length > 1) {
+        const element = arr.shift();
+        if(element === 's') {
+            result.push(0);
+            arr.push('s');
+        } else {
+            arr.push(...element.children);
+            result[result.length-1]++;
+        }
+    }
+    return result;
+}
 
 module.exports = levelWidth;
